16bd0f3ab8fa7f81b4e4aa3cee2baf879fd1f075
Jonathan Lam <jlam55555@gmail.com>
2020-04-01 00:17:43 -0400

    added basic react project files

---
+++ src/components/Authenticator/Authenticator.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Authenticator.css';
+
+const Authenticator = () => (
+  <div className="Authenticator">
+    <div>
+      <h1>Log in</h1>
+      Username <input type='text'/><br/>
+      Password <input type='password'/><br/>
+    </div>
+  
+  </div>
+);
+
+export default Authenticator;
