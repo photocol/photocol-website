2eaadef1547e55fd8ee2c689d7fba0eef223f9bf
R Lee <RL957is@gmail.com>
2020-04-07 03:22:53 -0400

    Made default function a lambda

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
 import './Photo.css';
+
 
 const Photo = () => (
   <div className="Photo">
     Photo Component
   </div>
 );
-
-
 
 Photo.propTypes = {};
 
