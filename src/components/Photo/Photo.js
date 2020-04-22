68bc4da2f70d3e0a3fbd4b97f233efa721897aa7
Jonathan Lam <jlam55555@gmail.com>
2020-04-22 12:17:06 -0400

    cleaned up photo.js

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -3,17 +3,6 @@ import PropTypes from 'prop-types';
 import './Photo.css';
 import {Link, withRouter} from 'react-router-dom';
 import {env} from "../../util/Environment";
-//import CollectionManager from "../../util/CollectionManager";
-
-/*
-const Photo = () => (
-
-    <div className="Photo">
-      Photo Component
-    </div>
-);
-*/
-
 
 class Photo extends React.Component {
   constructor(props) {
@@ -23,10 +12,6 @@ class Photo extends React.Component {
     this.state = {
       photouri,
     };
-  }
-
-  // putting this in componentDidMount() hook to be able to call setState properly
-  componentDidMount() {
   }
 
   render = () => {
