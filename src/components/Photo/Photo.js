2e3ba670c151458df9f3e279c916402b22e83062
Jonathan Lam <jlam55555@gmail.com>
2020-05-11 17:40:13 -0400

    added photo request to api

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Photo.css';
 import {Link, withRouter} from 'react-router-dom';
 import {Button} from "reactstrap";
+import ApiConnectionManager from "../../util/ApiConnectionManager";
 
 class Photo extends React.Component {
   constructor(props) {
@@ -11,7 +12,21 @@ class Photo extends React.Component {
     this.state = {
       photouri,
     };
+
+    this.acm = new ApiConnectionManager();
   }
+
+  componentDidMount = () => {
+    this.acm.request(`/photo/${this.state.photouri}/details`, {
+      method: 'POST'
+    })
+      .then(res => {
+        console.log(res.response);
+      })
+      .catch(err => {
+        console.error(err.response.error);
+      })
+  };
 
   render = () => {
     return (<div>
@@ -20,8 +35,8 @@ class Photo extends React.Component {
       </Link>
       <div class='fill-screen'><img className="photo333" src={`${process.env.REACT_APP_SERVER_URL}/perma/${this.state.photouri}`}/></div>
     </div>);
-  }
-};
+  };
+}
 
 
 Photo.propTypes = {};
